895d921415d67e084a8530b958d1a53941da17aa
WillDafCom2 <willianpaulo_09@hotmail.com>
2019-10-05 14:42:46 -0300

    Challenge CRUD (Update and delete) & querys & connection DB MySQL

--- my_app/views.py
+++ my_app/views.py
@@ -51,3 +51,71 @@ def cria():
     connection.close()
 
     return redirect(url_for('contas.lista_contas'))
+
+'''PEGA CONTA NO BD'''
+@conta_bp.route('/form/<int:id>/edit', methods=['GET'])
+def get_update(id):
+
+    update = True
+
+    connection = get_connection()
+
+    sql = 'select * from contas where id={}'.format(int(id))
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    resultado = cursor.fetchall()
+
+    id = resultado[0][0]
+    numero = resultado[0][1]
+    titular = resultado[0][2]
+    saldo = resultado[0][3]
+    limite = resultado[0][4]
+
+    connection.close()
+
+    return render_template('form.html',
+                           id=id,
+                           numero=numero,
+                           titular=titular,
+                           saldo=saldo,
+                           limite=limite,
+                           update=update)
+
+'''ATUALIZACAO DA CONTA'''
+@conta_bp.route('/<int:id>/edit', methods=['POST'])
+def update(id):
+
+    connection = get_connection()
+
+    numero = request.form.get('numero')
+    titular = request.form.get('titular')
+    saldo = float(request.form.get('saldo'))
+    limite = float(request.form.get('limite'))
+
+    sql = 'update contas set numero=%s, titular=%s, saldo=%s, limite=%s where id=%s;'
+    valores = (numero, titular, saldo, limite, int(id))
+
+    cursor = connection.cursor()
+    cursor.execute(sql, valores)
+
+    connection.commit()
+    connection.close()
+
+    return redirect(url_for('contas.lista_contas'))
+
+'''DELETA CONTA'''
+@conta_bp.route('/<int:id>/remove')
+def remove(id):
+
+    connection = get_connection()
+
+    sql = 'delete from contas where id={}'.format(int(id))
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+
+    connection.commit()
+    connection.close()
+
+    return redirect(url_for('contas.lista_contas'))
